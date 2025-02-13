b3ec8368c47be6efccf40c4b0eca4340ef19cd0d
Stepan Valuev <litovets008@gmail.com>
2025-02-13 18:07:12 +0300

    update bubble sort function

--- task01/main.cpp
+++ task01/main.cpp
@@ -2,17 +2,20 @@
 #include "sort.h"
 #include <ctime>
 
-#define SIZE 20
+#define SIZE 10
 
-// Это мощнй комментарий
+
 int main() {
-	srand(time(NULL));
-	int array[SIZE];
+	//srand(time(NULL));
+	int array[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	//int array[]{10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	init(array, SIZE, -20, 20);
+	//init(array, SIZE, -20, 20);
 
 	cout << "Before array: " << convert(array, SIZE) << endl;
+
 	bubble_sort(array, SIZE);
+
 	cout << "After array: " << convert(array, SIZE) << endl;
 
 
--- task01/sort.cpp
+++ task01/sort.cpp
@@ -1,17 +1,27 @@
+#include "util.h"
 #include "sort.h"
-
+//O(N^2)
+//best-O(N)
 void bubble_sort(int* array, int size) {
+	int count = 0;
 	for (int g = 0; g < size - 1; g++)
 	{
-		for (int i = 0; i < size - 1; i++)
+		bool flag = true;
+		for (int i = 0; i < size - 1 - g; i++)
 		{
 			if (array[i] > array[i + 1]) {
 				int t = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = t;
+				flag = false;
 			}
+			count++;
 		}
+		if (flag)
+		{
+			break;
+		}
+	
 	}
-
 
 }
